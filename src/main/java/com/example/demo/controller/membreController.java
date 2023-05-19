63fd2558b8579b64b3d83530c800742b626b7435
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-05-19 17:41:48 +0000

    modified:   src/main/java/com/example/demo/controller/membreController.java 	modified:   src/main/java/com/example/demo/service/membreService.java

--- src/main/java/com/example/demo/controller/membreController.java
+++ src/main/java/com/example/demo/controller/membreController.java
@@ -5,33 +5,53 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Membre;
-import com.example.demo.repository.membreRepository;
+import com.example.demo.service.membreService;
 
 @RestController
 @RequestMapping("/members")
 public class membreController {
     
     @Autowired
-    private membreRepository memRepo;
+    private membreService memService;
 
     @GetMapping
     public ResponseEntity<List<Membre>> getAllMembres(){
-        List<Membre> membres = memRepo.findAll();
+        List<Membre> membres = memService.getAllMembres();
         return new ResponseEntity<>(membres, HttpStatus.OK);
     }
 
     @PostMapping("/add")
     public ResponseEntity<Membre> addMember(@RequestBody Membre membre){
-        memRepo.save(membre);
-        return new ResponseEntity<>(membre, HttpStatus.CREATED);
+        Membre newMembre = memService.addMembre(membre);
+        return new ResponseEntity<>(newMembre, HttpStatus.CREATED);
     }
 
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Membre> editMember(@PathVariable Integer id, @RequestBody Membre membre){
+        Membre exMembre = memService.updateMembre(membre, id);
+        if(exMembre != null)
+            return new ResponseEntity<>(exMembre, HttpStatus.OK);
+        else 
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Membre> deleteMembre(@PathVariable Integer id){
+        boolean deletedMembre = memService.deleteMembre(id);
+        if(deletedMembre)
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        else 
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
 }
--- src/main/java/com/example/demo/service/membreService.java
+++ src/main/java/com/example/demo/service/membreService.java
@@ -1,8 +1,47 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.membreRepository;
+
+import com.example.demo.model.Membre;
 
 @Service
 public class membreService {
+
+    @Autowired
+    private membreRepository memRepo;
+
+    public List<Membre> getAllMembres(){
+        return memRepo.findAll();
+    }
+
+    public Membre getMembre(Integer id){
+        return memRepo.findById(id).orElse(null);
+    }
+
+    public Membre addMembre(Membre membre){
+        return memRepo.save(membre);
+    }
+
+    public Membre updateMembre(Membre membre, Integer id){
+        Membre exMembre = memRepo.findById(id).orElse(null);
+        if(exMembre != null) {
+           exMembre.setNom(membre.getNom());
+           exMembre.setPrenom(membre.getPrenom());
+           exMembre.setEmail(membre.getEmail());
+           exMembre.setDirector(membre.isDirector());
+           return memRepo.save(exMembre);
+        } else return null;
+    }
     
+    public boolean deleteMembre(Integer id){
+        if(memRepo.existsById(id)){
+            memRepo.deleteById(id);
+            return true;
+        } else return false;
+    }
 }
