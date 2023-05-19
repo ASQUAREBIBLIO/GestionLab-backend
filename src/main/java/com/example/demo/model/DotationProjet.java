0cdaab9e352fbeebf5a0dea5cf781a3153b4496f
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-05-19 16:08:31 +0000

    new file:   src/main/java/com/example/demo/controller/adminController.java 	modified:   src/main/java/com/example/demo/model/DotationProjet.java

--- src/main/java/com/example/demo/model/DotationProjet.java
+++ src/main/java/com/example/demo/model/DotationProjet.java
@@ -2,40 +2,25 @@ package com.example.demo.model;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
+// setters and getters
+// constructors with an without args
+@Setter @Getter
+@NoArgsConstructor @AllArgsConstructor
 public class DotationProjet {
     @EmbeddedId
     private DotationProjetPK DotationProjetpk;
     
     private double dotation;
     private boolean isRespoProjet;
-	public DotationProjet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 	public DotationProjetPK getDotationProjetpk() {
 		return DotationProjetpk;
 	}
-	public void setDotationProjetpk(DotationProjetPK dotationProjetpk) {
-		DotationProjetpk = dotationProjetpk;
-	}
-	public double getDotation() {
-		return dotation;
-	}
-	public void setDotation(double dotation) {
-		this.dotation = dotation;
-	}
-	public boolean isRespoProjet() {
-		return isRespoProjet;
-	}
-	public void setRespoProjet(boolean isRespoProjet) {
-		this.isRespoProjet = isRespoProjet;
-	}
-    
-    
-    
-    
     
 }
