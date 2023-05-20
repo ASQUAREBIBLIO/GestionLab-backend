9256d82c92fe33d4c208fd4a8a69e1288dab4f60
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-05-20 11:53:05 +0000

    modified:   src/main/java/com/example/demo/service/etablissementService.java

--- src/main/java/com/example/demo/service/etablissementService.java
+++ src/main/java/com/example/demo/service/etablissementService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.etablissementRepository;
 
-import jakarta.transaction.Transactional;
-
 import com.example.demo.model.Etablissement;
 
 @Service
