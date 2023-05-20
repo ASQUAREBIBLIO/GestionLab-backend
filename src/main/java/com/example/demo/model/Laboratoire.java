19cdbcb868715e6e560b572d9005a2ee967cc369
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-05-20 11:46:15 +0000

    modified:   src/main/java/com/example/demo/model/Laboratoire.java

--- src/main/java/com/example/demo/model/Laboratoire.java
+++ src/main/java/com/example/demo/model/Laboratoire.java
@@ -3,9 +3,11 @@ package com.example.demo.model;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -23,7 +25,8 @@ public class Laboratoire {
     
     private String nomLabo;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn (name = "etablissement_id")
     private Etablissement etablissement;
     
     @OneToMany(mappedBy="laboratoire")
