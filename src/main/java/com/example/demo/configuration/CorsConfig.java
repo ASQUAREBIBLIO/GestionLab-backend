e180728c0cbb7d80bba892d20720f9ae78a8a073
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 15:31:02 +0000

    modified:   src/main/java/com/example/demo/configuration/CorsConfig.java

--- src/main/java/com/example/demo/configuration/CorsConfig.java
+++ src/main/java/com/example/demo/configuration/CorsConfig.java
@@ -1,42 +1,27 @@
 package com.example.demo.configuration;
 
-import java.util.Collections;
-
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+    
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        
-        // Allow requests from all origins
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        
-        // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-        config.addAllowedMethod(HttpMethod.GET);
-        config.addAllowedMethod(HttpMethod.POST);
-        config.addAllowedMethod(HttpMethod.PUT);
-        config.addAllowedMethod(HttpMethod.DELETE);
-        config.addAllowedMethod(HttpMethod.OPTIONS); // Required for pre-flight requests
-        
-        // Allow all headers
-        config.addAllowedHeader("*");
-        
-        // Allow cookies to be sent
-        config.setAllowCredentials(true);
-        
-        // Set the maximum age of the cached pre-flight response in seconds
-        config.setMaxAge(3600L);
-        
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+	CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*")); // Use setAllowedOriginPatterns instead of
+																		// setAllowedOrigins
+		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
 }
 
