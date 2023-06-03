3d34f9afd0bb648356d7f22796ad9aca3defe5b3
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 15:03:09 +0000

    modified:   src/main/java/com/example/demo/controller/membreController.java

--- src/main/java/com/example/demo/controller/membreController.java
+++ src/main/java/com/example/demo/controller/membreController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.example.demo.service.membreService;
 import lombok.AllArgsConstructor;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8082/membres")
 @RequestMapping("/membres")
 @AllArgsConstructor
 public class membreController {
