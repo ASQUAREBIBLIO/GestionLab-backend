37f9cec5f7b8890e2765c1e71230ba5cf4fdd7f6
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-10 21:21:57 +0000

    modified:   src/main/java/com/example/demo/controller/AdminController.java

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,12 @@ import lombok.AllArgsConstructor;
 public class AdminController {
     private AdminRepository adminRepository;
 
-    @PostMapping("/regiter")
+    @GetMapping("/{id}")
+    public ResponseEntity<Admin> getAdminById(@PathVariable("id") Integer adminId){
+        return new ResponseEntity<>(adminRepository.findById(adminId).get(), HttpStatus.OK);
+    }
+
+    @PostMapping
     public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin){
         return new ResponseEntity<>(adminRepository.save(admin), HttpStatus.CREATED);
     }
